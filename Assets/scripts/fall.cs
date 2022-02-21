c2f75758502737aeb3b7e646267168a431d91e06
Diana <noah940407@gmail.com>
2022-02-21 20:08:08 +0800

    simple movement and stage fixed

--- Assets/scripts/fall.cs
+++ Assets/scripts/fall.cs
@@ -5,25 +5,41 @@ using UnityEngine;
 public class fall : MonoBehaviour
 {   
     public float fallspeed;
-    private string test;
+    private Rigidbody2D rb;
+    private bool destroy;
     
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        if(transform.position.y <= -5 && destroy == false){
+            destroy = true;
+            Invoke("I",10);
+        }
     }
     private void OnCollisionStay2D(Collision2D other) {
         if(other.gameObject.tag == "player"){
-            InvokeRepeating("falldown",2,1);
+            Invoke("falldown",2);
         }
     }
     void falldown(){
-        this.transform.Translate(new Vector2(0,-fallspeed)*Time.deltaTime);
+        rb.isKinematic = false;
+        rb.constraints = RigidbodyConstraints2D.None;
     }
+    void I(){
+        //destroy = true;
+        rb.isKinematic = true;
+        Instantiate(this.gameObject, new Vector2(transform.position.x, -2), Quaternion.identity);
+        D();
+        destroy = true;
+    }
+    void D(){
+        Destroy(this.gameObject);
+    }
+   
 }
---
+++ Assets/scripts/movement_1P.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement_1P : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    public float jump_force;
+    private int jt = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A)){
+            rb.velocity = new Vector2(-speed,rb.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            rb.velocity = new Vector2(speed,rb.velocity.y);
+        }
+        else{
+            rb.velocity = new Vector2(0,rb.velocity.y);
+        }
+        if(Input.GetKeyDown(KeyCode.W) && jt > 0){
+            rb.AddForce(new Vector2(0,jump_force),ForceMode2D.Impulse);
+            jt--;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "floor"){
+            jt = 2;
+        }
+    }
+}
